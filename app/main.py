1d4e1bc8284f51f70196d8f7cd59d5565578189b
Dan Sapp <dan.sapp@storable.com>
2026-05-14 12:46:22 -0500

    vessels endpoint w/ static data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
@@ -12,6 +12,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+with open("app/vessels.json") as f:
+    vessels = json.load(f)
+
 @app.get("/api/vessels")
-def get_vessels():
-    return [{"id": "test", "lat": 0, "lon": 0}]
+def get_vessels(
+    west: float = Query(...),
+    south: float = Query(...),
+    east: float = Query(...),
+    north: float = Query(...)
+):
+    # filter latitude
+    def in_bounds(v):
+        lat_ok = south <= v["lat"] <= north
+
+        # handle dateline wrap
+        if west <= east:
+            lon_ok = west <= v["lon"] <= east
+        else:
+            lon_ok = v["lon"] >= west or v["lon"] <= east
+
+        return lat_ok and lon_ok
+
+    filtered = list(filter(in_bounds, vessels))
+
+    return filtered[:500]
