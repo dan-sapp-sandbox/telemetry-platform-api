83464aab64aa88ece72c7cc84a19a36fec09d08a
Dan Sapp <dan.sapp@storable.com>
2026-05-14 12:39:40 -0500

    cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://www.dan-sapp.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/vessels")
 def get_vessels():
